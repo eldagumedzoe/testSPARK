6ecb09be73d682b48495a54c4ee066d3d4b84f5c
eldagumedzoe <61330655+eldagumedzoe@users.noreply.github.com>
2020-02-21 20:50:31 +0100

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,13 @@
+1)Authentification
+-You must enter your email and passeword
+-Then you have to presss the button "Login" to access the page of BMI Calculator
+NB:The email and the password you must be used is:
+email : admin@gmail.com
+password : awokou
+2)The page of BMI Calculator
+-You must choose the unit in which the height will be
+-You must choose the unit in which the weight will be
+-In others words you will enter your height in meters and your weight in kilograms
+-then you have to press the calculate button
+-The calculate button will then give you a result and a message
+-The message will explain to you if you are lean or overweight depending on your height in relation to your weight
---
+++ bmicalculator.sql
@@ -0,0 +1,119 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.9
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le :  ven. 21 fév. 2020 à 15:35
+-- Version du serveur :  5.7.21
+-- Version de PHP :  7.1.16
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `bmicalculator`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `converters`
+--
+
+DROP TABLE IF EXISTS `converters`;
+CREATE TABLE IF NOT EXISTS `converters` (
+  `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `height` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `weight` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `total_bmi` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `isreset` tinyint(1) NOT NULL DEFAULT '0',
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `converters`
+--
+
+INSERT INTO `converters` (`id`, `height`, `weight`, `total_bmi`, `isreset`, `created_at`, `updated_at`) VALUES
+(1, '1', '50', '50', 0, '2020-02-21 04:42:24', '2020-02-21 04:42:24'),
+(2, '2', '50', '12.5', 0, '2020-02-21 05:13:28', '2020-02-21 05:13:28'),
+(3, '2', '90', '22.5', 0, '2020-02-21 08:43:09', '2020-02-21 08:43:09');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `migrations`
+--
+
+DROP TABLE IF EXISTS `migrations`;
+CREATE TABLE IF NOT EXISTS `migrations` (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(1, '2014_10_12_000000_create_users_table', 1),
+(2, '2014_10_12_100000_create_password_resets_table', 1),
+(3, '2020_02_20_131311_create_converters_table', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `password_resets`
+--
+
+DROP TABLE IF EXISTS `password_resets`;
+CREATE TABLE IF NOT EXISTS `password_resets` (
+  `email` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  KEY `password_resets_email_index` (`email`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `users_email_unique` (`email`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'Elda', 'admin@gmail.com', '2020-02-20 23:00:00', '$2y$10$jFkgw7OVBx9K/U1XoWo80.7PxJh8eu1dwLpnKPJJ4JpfFQ43BMlQ.', NULL, '2020-02-20 23:00:00', '2020-02-20 23:00:00');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
